382aa4eefadd1fa8ff75bee5ffb289c24434c9a2
Emerson Leite <emerson.bl@gmail.com>
2024-01-16 19:40:17 -0300

    chore: add files

---
+++ cmd/api/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+
+	g := gin.Default()
+
+	g.GET("/", func(ctx *gin.Context) {
+		ctx.JSON(200, gin.H{
+			"message": "Hello",
+		})
+	})
+
+	g.Run(":3000")
+
+}
